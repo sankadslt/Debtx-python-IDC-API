458bd833e45b053a8c96373522561f41b065ba69
Sandani Gunawardhana <135797613+GunawardhanaSIA@users.noreply.github.com>
2025-05-08 11:47:39 +0530

    Delete Get_Case_Phase_API directory

--- Get_Case_Phase_API/utils/customerExceptions/customerExceptions.py
+++
@@ -1,9 +0,0 @@
-# custom_exceptions.py
-
-class TaskProcessingError(Exception):
-    """Base exception for task processing errors."""
-    pass
-
-class DatabaseConnectionError(TaskProcessingError):
-    """Raised when there is an issue connecting to the database."""
-    pass
