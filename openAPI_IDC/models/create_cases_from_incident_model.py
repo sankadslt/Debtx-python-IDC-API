fa11cb23b549f1b4f692b522d622312d172890ac
Iman Chandrasiri <iman.20221928@iit.ac.lk>
2025-06-13 10:56:22 +0530

    model updated

--- openAPI_IDC/models/create_cases_from_incident_model.py
+++ openAPI_IDC/models/create_cases_from_incident_model.py
@@ -6,4 +6,4 @@ from datetime import datetime
 class CreateCaseResponse(BaseModel):
     detail: str
     incident_id: int
-    case_id: int | str  # depending on your case_id format
+    case_id: int 
